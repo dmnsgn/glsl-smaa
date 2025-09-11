23d04fbe80d911fbcb6eca267a0cf55ec2f95e4c
Damien Seguin <dmnsgn@gmail.com>
2025-09-11 15:16:27 +0100

    refactor: reorder edges types

--- smaa-edges.frag.js
+++ smaa-edges.frag.js
@@ -45,6 +45,50 @@ varying vec4 vOffset[3];
   }
 #endif
 
+#ifdef SMAA_EDGES_LUMA
+  vec2 SMAALumaEdgeDetection(vec2 texcoord, vec4 offset[3], sampler2D colorTex) {
+    vec2 threshold = vec2(SMAA_THRESHOLD);
+
+    // Calculate lumas:
+    vec3 weights = vec3(0.2126, 0.7152, 0.0722);
+    float L = dot(texture2D(colorTex, texcoord).rgb, weights);
+
+    float Lleft = dot(texture2D(colorTex, offset[0].xy).rgb, weights);
+    float Ltop  = dot(texture2D(colorTex, offset[0].zw).rgb, weights);
+
+    // We do the usual threshold:
+    vec4 delta;
+    delta.xy = abs(L - vec2(Lleft, Ltop));
+    vec2 edges = step(threshold, delta.xy);
+
+    // Then discard if there is no edge:
+    if (dot(edges, vec2(1.0, 1.0)) == 0.0)
+        discard;
+
+    // Calculate right and bottom deltas:
+    float Lright = dot(texture2D(colorTex, offset[1].xy).rgb, weights);
+    float Lbottom  = dot(texture2D(colorTex, offset[1].zw).rgb, weights);
+    delta.zw = abs(L - vec2(Lright, Lbottom));
+
+    // Calculate the maximum delta in the direct neighborhood:
+    vec2 maxDelta = max(delta.xy, delta.zw);
+
+    // Calculate left-left and top-top deltas:
+    float Lleftleft = dot(texture2D(colorTex, offset[2].xy).rgb, weights);
+    float Ltoptop = dot(texture2D(colorTex, offset[2].zw).rgb, weights);
+    delta.zw = abs(vec2(Lleft, Ltop) - vec2(Lleftleft, Ltoptop));
+
+    // Calculate the final maximum delta:
+    maxDelta = max(maxDelta.xy, delta.zw);
+    float finalDelta = max(maxDelta.x, maxDelta.y);
+
+    // Local contrast adaptation:
+    edges.xy *= step(finalDelta, SMAA_LOCAL_CONTRAST_ADAPTATION_FACTOR * delta.xy);
+
+    return edges;
+  }
+#endif
+
 #ifdef SMAA_EDGES_COLOR
   vec2 SMAAColorEdgeDetection(vec2 texcoord, vec4 offset[3], sampler2D colorTex) {
     // Calculate the threshold:
@@ -101,59 +145,15 @@ varying vec4 vOffset[3];
   }
 #endif
 
-#ifdef SMAA_EDGES_LUMA
-  vec2 SMAALumaEdgeDetection(vec2 texcoord, vec4 offset[3], sampler2D colorTex) {
-    vec2 threshold = vec2(SMAA_THRESHOLD);
-
-    // Calculate lumas:
-    vec3 weights = vec3(0.2126, 0.7152, 0.0722);
-    float L = dot(texture2D(colorTex, texcoord).rgb, weights);
-
-    float Lleft = dot(texture2D(colorTex, offset[0].xy).rgb, weights);
-    float Ltop  = dot(texture2D(colorTex, offset[0].zw).rgb, weights);
-
-    // We do the usual threshold:
-    vec4 delta;
-    delta.xy = abs(L - vec2(Lleft, Ltop));
-    vec2 edges = step(threshold, delta.xy);
-
-    // Then discard if there is no edge:
-    if (dot(edges, vec2(1.0, 1.0)) == 0.0)
-        discard;
-
-    // Calculate right and bottom deltas:
-    float Lright = dot(texture2D(colorTex, offset[1].xy).rgb, weights);
-    float Lbottom  = dot(texture2D(colorTex, offset[1].zw).rgb, weights);
-    delta.zw = abs(L - vec2(Lright, Lbottom));
-
-    // Calculate the maximum delta in the direct neighborhood:
-    vec2 maxDelta = max(delta.xy, delta.zw);
-
-    // Calculate left-left and top-top deltas:
-    float Lleftleft = dot(texture2D(colorTex, offset[2].xy).rgb, weights);
-    float Ltoptop = dot(texture2D(colorTex, offset[2].zw).rgb, weights);
-    delta.zw = abs(vec2(Lleft, Ltop) - vec2(Lleftleft, Ltoptop));
-
-    // Calculate the final maximum delta:
-    maxDelta = max(maxDelta.xy, delta.zw);
-    float finalDelta = max(maxDelta.x, maxDelta.y);
-
-    // Local contrast adaptation:
-    edges.xy *= step(finalDelta, SMAA_LOCAL_CONTRAST_ADAPTATION_FACTOR * delta.xy);
-
-    return edges;
-  }
-#endif
-
 void main() {
   vec4 color;
 
   #ifdef SMAA_EDGES_DEPTH
     color.xy = SMAADepthEdgeDetection(vTexCoord0, vOffset, uDepthTexture);
-  #elif defined(SMAA_EDGES_COLOR)
-    color.xy = SMAAColorEdgeDetection(vTexCoord0, vOffset, uColorTexture);
   #elif defined(SMAA_EDGES_LUMA)
     color.xy = SMAALumaEdgeDetection(vTexCoord0, vOffset, uColorTexture);
+  #elif defined(SMAA_EDGES_COLOR)
+    color.xy = SMAAColorEdgeDetection(vTexCoord0, vOffset, uColorTexture);
   #endif
 
   gl_FragColor = color;
